53fc6a9dd8481380ea9d880c7f43b9882ca0cd3a
jakeau <reidjako1111@gmail.com>
2020-07-27 09:48:30 +1000

    abv

--- _upload/packages.php
+++ _upload/packages.php
@@ -5,17 +5,25 @@ $packages = array();
 $packages[] = array(
     'name' => 'ahs189',
     'title' => 'AHS 1.8.9 PvP',
-    'version' => '2020-05-23-00-17-24',
+    'version' => '2020-07-27-08-04-59',
     'priority' => 100,
     'location' => 'ahs189.json',
 );
 
 $packages[] = array(
-    'name' => 'RougelikeAdventures1122',
-    'title' => 'Rougelike Adventures 1.12.2',
-    'version' => '2020-05-23-00-16-37',
+    'name' => 'nightmare_industries',
+    'title' => 'Nightmare Industries',
+    'version' => '2020-07-27-08-05-50',
+    'priority' => 90,
+    'location' => 'nightmare_industries.json',
+);
+
+$packages[] = array(
+    'name' => 'labymod',
+    'title' => 'LMod 1.8.9',
+    'version' => '2020-07-27-08-04-53',
     'priority' => 0,
-    'location' => 'rougelikeadventures1122.json',
+    'location' => 'labymod.json',
 );
 
 $out = array('minimumVersion' => 1, 'packages' => $packages);
